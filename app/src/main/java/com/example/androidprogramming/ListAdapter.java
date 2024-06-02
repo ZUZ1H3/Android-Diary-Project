42c825e61c09810082de025de713f7377e1872f6
jujihye <dya_yaaa@naver.com>
2024-06-02 17:36:30 +0900

    리스트 액티비티에 날씨와 기분 이미지가 뜨도록 수정함

--- app/src/main/java/com/example/androidprogramming/ListAdapter.java
+++ app/src/main/java/com/example/androidprogramming/ListAdapter.java
@@ -5,20 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
 public class ListAdapter extends BaseAdapter {
 
     private Context context;
-    private ArrayList<Diary> diaryList; // Diary 객체의 ArrayList를 사용합니다.
+    private ArrayList<Diary> diaryList;
 
     public ListAdapter(Context context, ArrayList<Diary> diaryList) {
         this.context = context;
@@ -48,12 +43,72 @@ public class ListAdapter extends BaseAdapter {
 
         TextView dateText = convertView.findViewById(R.id.dateText);
         TextView diaryText = convertView.findViewById(R.id.diaryText);
+        ImageView weatherImg = convertView.findViewById(R.id.weatherImg);
+        ImageView moodImg = convertView.findViewById(R.id.moodImg);
 
         Diary diary = diaryList.get(position);
 
-        // 날짜와 일기 내용을 설정합니다.
+        // 날짜와 일기 내용 설정
         dateText.setText(diary.getDate2(diary.getDate()));
         diaryText.setText(diary.getContent());
+
+        // 날씨 이미지 설정
+        switch (diary.getWeather()) {
+            case "sunny":
+                weatherImg.setImageResource(R.drawable.weather_sunny);
+                break;
+            case "rainy":
+                weatherImg.setImageResource(R.drawable.weather_rainy);
+                break;
+            case "cloudy":
+                weatherImg.setImageResource(R.drawable.weather_cloudy);
+                break;
+            case "windy":
+                weatherImg.setImageResource(R.drawable.weather_windy);
+                break;
+            case "lightning":
+                weatherImg.setImageResource(R.drawable.weather_lightning);
+                break;
+            case "snowy":
+                weatherImg.setImageResource(R.drawable.weather_snowy);
+                break;
+            case "rainbow":
+                weatherImg.setImageResource(R.drawable.weather_rainbow);
+                break;
+            // 이하 날씨 이미지 추가
+        }
+
+        // 기분 이미지 설정
+        switch (diary.getMood()) {
+            case "happy":
+                moodImg.setImageResource(R.drawable.mood_happy);
+                break;
+            case "sad":
+                moodImg.setImageResource(R.drawable.mood_sad);
+                break;
+            case "angry":
+                moodImg.setImageResource(R.drawable.mood_angry);
+                break;
+            case "lucky":
+                moodImg.setImageResource(R.drawable.mood_lucky);
+                break;
+            case "soso":
+                moodImg.setImageResource(R.drawable.mood_soso);
+                break;
+            case "flutter":
+                moodImg.setImageResource(R.drawable.mood_flutter);
+                break;
+            case "tired":
+                moodImg.setImageResource(R.drawable.mood_tired);
+                break;
+            case "comfortable":
+                moodImg.setImageResource(R.drawable.mood_comfortable);
+                break;
+            case "exciting":
+                moodImg.setImageResource(R.drawable.mood_exciting);
+                break;
+            // 이하 기분 이미지 추가
+        }
 
         return convertView;
     }
